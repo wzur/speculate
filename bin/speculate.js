2f65ef4afb841c7be3292d5e7f6fe902830856da
Chris Sidebottom <chris.sidebottom@bbc.co.uk>
2016-03-10 10:19:21 +0000

    Add clean module to remove old files

--- bin/speculate.js
+++ bin/speculate.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var validator = require('../lib/validator');
 var generate = require('../lib/generate');
+var clean = require('../lib/clean');
 var cwd = process.cwd();
 
 var isValid = validator(cwd);
@@ -14,6 +15,7 @@ if (!isValid) {
 
 var pkg = require(path.resolve(cwd, './package.json'));
 
+clean(cwd, pkg);
 generate(cwd, pkg, function (err) {
   console.log('hai guiz');
 
---
+++ test/clean.js
@@ -0,0 +1,42 @@
+var sinon = require('sinon');
+var fs = require('fs');
+var rimraf = require('rimraf');
+var clean = require('../lib/clean');
+
+var pkg = require('./fixtures/my-cool-api');
+var sandbox = sinon.sandbox.create();
+
+describe('clean', function () {
+  beforeEach(function () {
+    sandbox.stub(rimraf, 'sync');
+    sandbox.stub(fs, 'unlinkSync');
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it('removes the existing service file', function () {
+    clean('/path/to/project', pkg);
+    sinon.assert.calledWith(
+      fs.unlinkSync,
+      '/path/to/project/my-cool-api.service'
+    );
+  });
+
+  it('removes the SPECS directory', function () {
+    clean('/path/to/project', pkg);
+    sinon.assert.calledWith(
+      rimraf.sync,
+      '/path/to/project/SPECS'
+    );
+  });
+
+  it('removes the SOURCES directory', function () {
+    clean('/path/to/project', pkg);
+    sinon.assert.calledWith(
+      rimraf.sync,
+      '/path/to/project/SOURCES'
+    );
+  });
+});
